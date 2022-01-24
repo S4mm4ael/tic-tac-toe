1cc8c53dae5a89e38689b03230282589095a1b6a
S4mm4el <homer1996@gmail.com>
2022-01-24 17:40:25 +0300

    feat: add hamburger menu JS part

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,8 +1,20 @@
 console.log("110 баллов: Все пункты выполнены полностью");
 
-$(document).ready(function(){
-    $(".hamburger").click(function(){
-      $(this).toggleClass("is-active");
-    });
-  });
-  
+const hamburger = document.querySelector('.hamburger');
+const menubox = document.querySelector('.menubox');
+
+
+function toggleMenu() {
+  hamburger.classList.toggle('open');
+  menubox.classList.toggle('open');
+}
+
+function closeMenu(event) {
+  if (event.target.classList.contains('nav-link')) {
+  hamburger.classList.remove('open');
+  menubox.classList.remove('open');
+  }
+}
+
+hamburger.addEventListener('click', toggleMenu);
+menubox.addEventListener('click', closeMenu);
