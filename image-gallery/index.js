17757b3faba260c9b02579264ede314c71d27ba7
Semion Krapivin <homer1996@gmail.com>
2022-02-13 19:40:07 +0300

    feat: add index.html

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+// My mark //
+console.log("Ваша отметка - 70 баллов");
+
+// Active button
+
+const btns = document.querySelectorAll('.btn');
+const play = document.querySelector('.pbtn');
+
